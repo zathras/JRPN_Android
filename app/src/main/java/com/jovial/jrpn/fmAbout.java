043bc4771f523ffa77d607cb4d8eeb4e0114982a
Bill Foote <billf@jovial.com>
2021-02-13 15:11:53 -0800

    More on issue #6:  Actually show the build date :-)

--- app/src/main/java/com/jovial/jrpn/fmAbout.java
+++ app/src/main/java/com/jovial/jrpn/fmAbout.java
@@ -14,20 +14,21 @@ import java.util.Date;
 
 //The "vanity plate"
 public class fmAbout extends Activity {
-	// I like to manually control the release date value
+    // I like to manually control the release date value
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-		TextView tvVersion = (TextView) findViewById(R.id.tvVersion);
-		tvVersion.setText("Version:  " + BuildConfig.VERSION_NAME);
-		TextView tvDate = (TextView) findViewById(R.id.tvDate);
-		tvDate.setText("Built:  " + (new SimpleDateFormat()).format(new Date()));
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+        TextView tvVersion = (TextView) findViewById(R.id.tvVersion);
+        tvVersion.setText("Version:  " + BuildConfig.VERSION_NAME);
+        TextView tvDate = (TextView) findViewById(R.id.tvDate);
+        tvDate.setText("Built:  " + (new SimpleDateFormat()).format(
+            new Date(BuildConfig.BUILD_TIMESTAMP)));
+    }
 
-	// let's blow this pop stand!
-	public void Close(View v) {
+    // let's blow this pop stand!
+    public void Close(View v) {
 		finish();
 	}
 }
